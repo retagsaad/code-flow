88ee0aede51e85755b7e8ded337d7d30e5b2ba82
retag <retagsaad77@gmail.com>
2026-03-03 23:57:30 +0200

    components

--- code flow/src/App.jsx
+++ code flow/src/App.jsx
@@ -1,11 +1,19 @@
-
 import './App.css'
+import Home from './components/Home'
+import Features from './components/Features'
+import Pricing from './components/Pricing'
+import AboutUs from './components/AboutUs'
 
 function App() {
 
   return (
     <>
-     
+     <div className='min-h-screen bg-slate-950 text-white overflow-hidden'>
+    <Home/>
+    <Features/>
+    <Pricing/>
+    <AboutUs/>
+     </div>
     </>
   )
 }
---
+++ code flow/src/components/PricingBox.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PricingBox() {
+  return (
+    <div>PricingBox</div>
+  )
+}

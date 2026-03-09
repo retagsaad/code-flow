f55aae4bf1c710cf96f80d7a3d7a9958fd6158f6
retag <retagsaad77@gmail.com>
2026-03-10 00:17:19 +0200

    update

--- code flow/src/components/Home.jsx
+++ code flow/src/components/Home.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Box from './Box'
 export default function Home() {
   return (
-    <div className='flex max-w-300 mx-auto px-5 mt-6'>
+    <div className='flex max-w-300 mx-auto px-5 mt-6 justify-between'>
       <div className='py-5 m-5'>
         <div className='bg-slate-900 w-48 p-1.5 rounded-full text-sm text-gray-400 mb-3'>
           <button>Introducing CodeFlow AI 2.0</button>
         </div>
         <h5 className='text-white font-bold text-5xl leading-snug'>Code Faster <br /> <span className='text-blue-400'>Build Better</span><br /> With CodeFlow AI</h5>
         <p className='w-120 text-sm text-gray-500 py-5'>Accelerate your development workflow with intelligent code completion automated testing, and smart debugging. Ship production-ready code 10x faster.</p>
-        <div className='py-5 flex'>
-          <button className='flex bg-blue-500 p-3 mr-7'><a href="#">Start Coding Free</a><img src="arrow.png" alt="" className='w-7'/></button>
-          <button className='flex'><img src="play_icon.png" alt="" className='w-10 h-10'/><a href="#">Watch Demo</a></button>
+        <div className='py-5 group-hover:bg-white/20 flex'>
+          <button  className='flex bg-blue-500 p-3 mr-7 rounded-xl mt-2 cursor-pointer'><a href="#" className='mr-3'>Start Coding Free</a><img src="arrow.png" alt="" className='cursor-pointer w-5 h-4 mt-1.5'/></button>
+          <button className='flex rounded-xl bg-gray-900 p-2 cursor-pointer'><img src="play_icon.png" alt="" className='w-10 h-10 cursor-pointer'/><a href="#" className='mt-2'>Watch Demo</a></button>
         </div>
       </div>
       <div>

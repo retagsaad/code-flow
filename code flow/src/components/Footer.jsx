d085258dcb485a6b86ad50cd6844bc909456b919
retag <retagsaad77@gmail.com>
2026-03-18 02:40:37 +0200

    footer

--- code flow/src/components/Footer.jsx
+++ code flow/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   const links = [
     {
-      title: "products",
+      title: "Products",
       items: ["Features", "Pricing", "Security", "Roadmap", "Changelog"],
     },
     {
@@ -26,29 +26,39 @@ export default function Footer() {
     },
   ];
   return (
-<div className="bg-[#050d31] text-white p-10">    
-    <div >
-        <img src="logo.png" alt="" />
-        <p>
-          Transform your workflow with AI-powered tools and automation. Built
-          for modern teams.
-        </p>
-        <div className="flex">
-        <a href="#" className="w-10"><img src="facebook.png" alt="" /></a>
-        <a href="#" className="w-10"><img src="instagram.png" alt="" /></a>
-        <a href="#" className="w-10"><img src="linkedin.png" alt="" /></a>
-        <a href="#" className="w-10"><img src="twitter.png" alt="" /></a>
+    <div className="bg-[#020a2d] text-white p-10">
+      <div className="grid grid-cols-[1.5fr_1fr_1fr_1fr_1fr] ">
+        <div className="col-span-1">
+          <img src="logo.png" alt="" className="w-40" />
+          <p className="text-gray-400 max-w-xs leading-relaxed ml-5">
+            Transform your workflow with AI-powered tools and automation. Built
+            for modern teams.
+          </p>
+          <div className="flex gap-2 my-5 ml-5">
+            <a href="#" className="w-6 h-7">
+              <img src="facebook.png" alt="" />
+            </a>
+            <a href="#" className="w-6">
+              <img src="instagram.png" alt="" />
+            </a>
+            <a href="#" className="w-6">
+              <img src="linkedin.png" alt="" />
+            </a>
+            <a href="#" className="w-6">
+              <img src="youtube.png" alt="" />
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-between ">
         {links.map((link, key) => (
           <div key={key}>
-            <h2 className="font-bold mb-3">{link.title}</h2>
-
+            <h2 className="font-bold  mb-3">{link.title}</h2>
             <ul className="space-y-2">
               {link.items.map((item, i) => (
                 <li key={i}>
-                  <a href="#" className="text-gray-400 hover:text-white">
+                  <a
+                    href="#"
+                    className="text-gray-400 hover:text-white transition"
+                  >
                     {item}
                   </a>
                 </li>
@@ -57,15 +67,21 @@ export default function Footer() {
           </div>
         ))}
       </div>
-      <hr className="text-gray-700 mt-5" />
+      <hr className="border-gray-700 my-5" />
       <div className="flex justify-between mt-5">
         <div>
           <p>© 2025 CodeFlow. All rights reserved.</p>
         </div>
         <div className="flex gap-5 ">
-          <a href="#" className="hover:text-gray-400">Privacy Policy</a>
-          <a href="#" className="hover:text-gray-400">Terms of Service</a>
-          <a href="#" className="hover:text-gray-400">Cookie Settings</a>
+          <a href="#" className="hover:text-gray-400 transition">
+            Privacy Policy
+          </a>
+          <a href="#" className="hover:text-gray-400 transition">
+            Terms of Service
+          </a>
+          <a href="#" className="hover:text-gray-400 transition">
+            Cookie Settings
+          </a>
         </div>
       </div>
     </div>
